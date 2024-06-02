ebd8f6a8411b09d5e26986d3b7cc525e49618104
Inti Villca <intivillca@gmail.com>
2024-06-02 12:41:34 +0200

    feat(db): auth middleware

--- apps/activity-db-server/src/index.ts
+++ apps/activity-db-server/src/index.ts
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import { routes } from "./routes";
+import { publicKeyMiddleware } from "./middleware/public-key.middleware";
+import { authMiddleware } from "./middleware/auth.middleware";
 
 dotenv.config({ path: ".env" });
 
@@ -11,6 +13,8 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use(publicKeyMiddleware);
+app.use(authMiddleware);
 app.use(routes);
 app.listen(port, () => {
   console.log(
