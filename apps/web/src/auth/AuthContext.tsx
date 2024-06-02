1995aab7bd4b8b170e04f2ec48feb9dcad37d45a
Inti Villca <intivillca@gmail.com>
2024-06-02 12:42:28 +0200

    feat(web): authcontext now only checks whether token exists, doesn't validate it (validation is done on remote services)

--- apps/web/src/auth/AuthContext.tsx
+++ apps/web/src/auth/AuthContext.tsx
@@ -1,37 +1,13 @@
-import { PropsWithChildren, createContext, useContext, useState } from "react";
-import { useQuery } from "react-query";
+import { PropsWithChildren, createContext, useContext } from "react";
 import { AuthContextValue } from "./types";
-import { verifyToken } from "./verifyToken";
-import { JWK } from "jose";
-import axios from "axios";
 
 const authContext = createContext<AuthContextValue | null>(null);
 
 export const AuthProvider = ({ children }: PropsWithChildren) => {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
   const token = window.localStorage.getItem("authToken") ?? "";
 
-  const { data: pubKey } = useQuery({
-    queryKey: ["AuthServer-pubkey"],
-    queryFn: async () => {
-      const data = await axios.get<JWK>("http://localhost:3002/pubkey");
-      return data.data;
-    },
-  });
-
-  useQuery({
-    queryKey: ["Auth-verifyToken", token],
-    queryFn: async () => {
-      if (token && pubKey) {
-        await verifyToken(token, pubKey);
-        setIsAuthenticated(true);
-      }
-    },
-    enabled: !!pubKey && !!token,
-  });
-
   return (
-    <authContext.Provider value={{ isAuthenticated }}>
+    <authContext.Provider value={{ isAuthenticated: !!token }}>
       {children}
     </authContext.Provider>
   );
